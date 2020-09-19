09aa7870e9f3b67746cb7534491285f72c14145d
Pedro Rodrigues <44656907+Rodriguespn@users.noreply.github.com>
2020-09-19 16:37:19 +0100

    minmax added

--- README.md
+++ README.md
@@ -8,8 +8,6 @@ Web page available at http://web.tecnico.ulisboa.pt/~ist190531/Tic-Tac-Toe/
 
 ## To-dos
 - Scoreboard durante o jogo
-- A home page were we can choose a 1 player game or 2 players game
-- Implement the minmax algorithm for the 1 player option
 - Implement a multiplayer environment
 
 ng build --prod --outputPath="Tic-Tac-Toe" --deployUrl="ist190531/Tic-Tac-Toe/"
--- app/src/app/board/board.component.ts
+++ app/src/app/board/board.component.ts
@@ -9,7 +9,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 export class BoardComponent implements OnInit {
   myForm: FormGroup;
-  begin: boolean = false;
+  begin = false;
 
   squares: any[];
   xIsNext: boolean;
@@ -66,9 +66,18 @@ export class BoardComponent implements OnInit {
     return this.player === 'X' ? this.playerOne.value : this.playerTwo.value;
   }
 
+  setPositionValue(idx: number, value): void {
+    this.squares.splice(idx, 1, value);
+  }
+
   makeMove(idx: number): void {
-    if (!this.squares[idx] && this.winner === null) {
-      this.squares.splice(idx, 1, this.player);
+    if (!this.winner && !this.squares[idx]) {
+        this.setPositionValue(idx, this.player);
+        this.xIsNext = !this.xIsNext;
+    }
+    this.winner = this.calculateWinner();
+    if (!this.twoPlayersGame.value && !this.winner) {
+      this.computerMove();
       this.xIsNext = !this.xIsNext;
     }
     this.winner = this.calculateWinner();
@@ -86,7 +95,6 @@ export class BoardComponent implements OnInit {
       [2, 4, 6],
     ];
 
-    // tslint:disable-next-line:prefer-for-of
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (this.squares[a] &&
@@ -101,7 +109,101 @@ export class BoardComponent implements OnInit {
         }
       }
     }
+
+    let tie = true;
+    for (let i = 0; i < this.squares.length; i++) {
+      if (!this.squares[i]) {
+        tie = false;
+      }
+    }
+
+    if (tie) {
+      return 'tie';
+    }
+
     return null;
   }
 
+  private computerMove(): void {
+    const minmaxConst = {
+      X: -1,
+      O: 1,
+      tie: 0,
+    };
+
+    function minmaxCalculateWinner(board): number {
+      const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+
+      for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (board[a] &&
+          board[a] === board[b] &&
+          board[a] === board[c]
+        ) {
+          return board[a];
+        }
+      }
+      return minmaxConst['tie'];
+    }
+
+    function minmax(board, alpha: number, beta: number, player: number): number {
+      // How is the position like for player (their turn) on board?
+      const winner: number = minmaxCalculateWinner(board);
+      if (winner !== minmaxConst['tie']) { return winner * player; }
+
+      let move = -1;
+      let score = -2; // Losing moves are preferred to no move
+      for (let i = 0; i < board.length; ++i) { // For all moves,
+        if (!board[i]) { // If legal,
+          board.splice(i, 1, player);
+          const thisScore: number = -minmax(board, alpha, beta, player * -1);
+          // Pick the one that's worst for the opponent
+          if (thisScore > score) {
+            score = thisScore;
+            move = i;
+          }
+          // Reset board after try
+          board.splice(i, 1, null);
+
+          if (score <= alpha) { return score; }
+          if (score < beta) {
+            beta = score;
+          }
+        }
+      }
+      if (move === -1) { return minmaxConst.tie; }
+      return score;
+    }
+
+    let x = -1;
+    let score = -2;
+    const board = new Array(9);
+    for (let i = 0; i < this.squares.length; ++i) {
+      if (this.squares[i]) {
+        board[i] = minmaxConst[this.squares[i]];
+      }
+    }
+    for (let i = 0; i < board.length; ++i) {
+      if (!board[i]) {
+        board.splice(i, 1, minmaxConst['O']);
+        const tempScore: number = -minmax(board, -2, 2, minmaxConst['X']);
+        board.splice(i, 1, null);
+        if (tempScore > score) {
+          score = tempScore;
+          x = i;
+        }
+      }
+    }
+    // returns a score based on minimax tree at a given node.
+    this.setPositionValue(x, 'O');
+  }
 }
